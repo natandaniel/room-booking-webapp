7557f2456a598ad3989165e22b3c7fbd1c56226e
natandaniel <natan.daniel0@gmail.com>
2018-07-23 22:45:24 +0200

    using PagingAndSortingRepository to enable hypermedia controls

--- src/main/java/com/natandanielapps/consensysbooking/repository/RoomRepository.java
+++ src/main/java/com/natandanielapps/consensysbooking/repository/RoomRepository.java
@@ -1,6 +1,6 @@
 package com.natandanielapps.consensysbooking.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.natandanielapps.consensysbooking.model.Room;
 
 @Repository
-public interface RoomRepository extends JpaRepository<Room, Long> {
+public interface RoomRepository extends PagingAndSortingRepository<Room, Long> {
 	
 	@SuppressWarnings("unchecked")
 	@Override
