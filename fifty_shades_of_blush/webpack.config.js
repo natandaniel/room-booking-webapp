d77759ade51e258da3a9c3f507556ee7e0bb284b
natan.daniel <natan.daniel0@gmail.com>
2019-07-14 18:36:09 +0200

    update using babel preset env

--- fifty_shades_of_blush/webpack.config.js
+++ fifty_shades_of_blush/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
                 loader: 'babel-loader',
                 query: {
                     cacheDirectory: true,
-                    presets: ['es2015', 'react']
+                    presets: ['@babel/preset-env', 'react']
                 }
             }
         ]
