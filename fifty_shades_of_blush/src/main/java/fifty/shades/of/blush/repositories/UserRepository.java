531e057ef2fa826595170d364de0cd3ada6b2df3
natandaniel <natan.daniel0@gmail.com>
2019-07-19 13:19:58 +0200

    removed @Repository

--- fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/UserRepository.java
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/UserRepository.java
@@ -1,11 +1,9 @@
 package fifty.shades.of.blush.repositories;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import fifty.shades.of.blush.entities.User;
 
-@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
 	User findByUsername(String username);
