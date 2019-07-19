d5f6824277345e104d4f29c1de47e88af68367ac
natandaniel <natan.daniel0@gmail.com>
2019-07-19 14:46:28 +0200

    adding test data

--- fifty_shades_of_blush/src/main/resources/data.sql
+++ fifty_shades_of_blush/src/main/resources/data.sql
@@ -1,2 +1,16 @@
 delete from articles;
 delete from users;
+
+insert into users (id, username, password, first_name, last_name, created_at, updated_at) values(1, 'natandaniel', 'natandaniel', 'natan', 'daniel', SYSDATE, SYSDATE);
+
+insert into articles (id, title, user_id, body, article_type, created_at, updated_at) 
+values (1, 'BEAUTY', 1, 'THIS IS A BEAUTY ARTICLE', 'BEAUTY', SYSDATE, SYSDATE);
+
+insert into articles (id, title, user_id, body, article_type, created_at, updated_at) 
+values (2, 'FASHION', 1, 'THIS IS A FASHION ARTICLE', 'FASHION', SYSDATE, SYSDATE);
+
+insert into articles (id, title, user_id, body, article_type, created_at, updated_at) 
+values (3, 'TRAVEL', 1, 'THIS IS A TRAVEL ARTICLE', 'TRAVEL', SYSDATE, SYSDATE);
+
+insert into articles (id, title, user_id, body, article_type, created_at, updated_at) 
+values (4, 'LIFESTYLE', 1, 'THIS IS A LIFESTYLE ARTICLE', 'LIFESTYLE', SYSDATE, SYSDATE);
