1757f488bedbb08c38408acbcfbe509725b35e85
natandaniel <natan.daniel0@gmail.com>
2019-07-19 10:41:14 +0200

    adding security config class

---
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/security/SecurityConfig.java
@@ -0,0 +1,11 @@
+package fifty.shades.of.blush.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+}
