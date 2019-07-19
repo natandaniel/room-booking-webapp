0e44e1bbf8224f9f7844602f6cbe2d7bbe738dbb
natandaniel <natan.daniel0@gmail.com>
2019-07-19 13:19:32 +0200

    updated repo to paging and sorting repo

--- fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/ArticleRepository.java
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/ArticleRepository.java
@@ -1,11 +1,9 @@
 package fifty.shades.of.blush.repositories;
 
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import fifty.shades.of.blush.entities.Article;
 
-@Repository
-public interface ArticleRepository extends CrudRepository<Article, Long> {
+public interface ArticleRepository extends PagingAndSortingRepository<Article, Long> {
 
 }
