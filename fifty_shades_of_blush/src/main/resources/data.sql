1c063d6abdfca6f9b474c15f4dce1c31ac6fda7a
natandaniel <natan.daniel0@gmail.com>
2019-07-19 13:30:45 +0200

    removing insert statements

--- fifty_shades_of_blush/src/main/resources/data.sql
+++ fifty_shades_of_blush/src/main/resources/data.sql
@@ -1,4 +1,2 @@
 delete from articles;
 delete from users;
-
-insert into users (id, username, password, first_name,last_name,created_at,updated_at) values (1,"totooo","titiii","totooo","titiii",SYSDATE,SYSDATE);
