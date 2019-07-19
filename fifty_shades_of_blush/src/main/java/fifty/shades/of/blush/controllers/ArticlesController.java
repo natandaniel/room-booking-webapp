c583c30d29a5f6df9eca86d281d40cf2235bc490
natandaniel <natan.daniel0@gmail.com>
2019-07-19 14:14:25 +0200

    adding put mapping and updating patch

--- fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/controllers/ArticlesController.java
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/controllers/ArticlesController.java
@@ -1,5 +1,7 @@
 package fifty.shades.of.blush.controllers;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -59,27 +62,44 @@ public class ArticlesController {
 		return new ResponseEntity<Iterable<Article>>(articles, HttpStatus.OK);
 	}
 
-	@PostMapping(path="/admin", consumes = "application/json")
+	@PostMapping(path = "/admin", consumes = "application/json")
 	@ResponseStatus(HttpStatus.CREATED)
 	public Article postTaco(@RequestBody Article article) {
 		return articleRepo.save(article);
 	}
 
+	@PutMapping("/admin/{articleId}")
+	public ResponseEntity<Article> putArticle(@RequestBody Article putArticle) {
+
+		Article article = articleRepo.save(putArticle);
+		return new ResponseEntity<>(article, HttpStatus.OK);
+	}
+
 	@PatchMapping(path = "/admin/{articleId}", consumes = "application/json")
-	public Article patchArticle(@PathVariable("articleId") Long articleId, @RequestBody Article patch) {
-		Article article = articleRepo.findById(articleId).get();
-		
-		if (patch.getTitle() != null) {
-			article.setTitle(patch.getTitle());
+	public ResponseEntity<Article> patchArticle(@PathVariable("articleId") Long articleId,
+			@RequestBody Article patchArticle) {
+
+		Optional<Article> optArticle = articleRepo.findById(articleId);
+
+		if (optArticle.isPresent()) {
+
+			Article article = optArticle.get();
+
+			if (patchArticle.getTitle() != null) {
+				article.setTitle(patchArticle.getTitle());
+			}
+			if (patchArticle.getBody() != null) {
+				article.setBody(patchArticle.getBody());
+			}
+			if (patchArticle.getType() != null) {
+				article.setType(patchArticle.getType());
+			}
+
+			return new ResponseEntity<Article>(articleRepo.save(article), HttpStatus.OK);
 		}
-		if (patch.getBody() != null) {
-			article.setBody(patch.getBody());
-		}
-		if (patch.getType() != null) {
-			article.setType(patch.getType());
-		}
-		
-		return articleRepo.save(article);
+
+		return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+
 	}
 
 }
