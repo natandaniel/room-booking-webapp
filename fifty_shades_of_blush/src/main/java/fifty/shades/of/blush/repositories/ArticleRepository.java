85707986ae1c81b88b9b622c92ca63f90c0c6326
natandaniel <natan.daniel0@gmail.com>
2019-07-19 12:42:00 +0200

    annotating repos with @Repository

--- fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/ArticleRepository.java
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/ArticleRepository.java
@@ -1,9 +1,11 @@
 package fifty.shades.of.blush.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import fifty.shades.of.blush.entities.Article;
 
+@Repository
 public interface ArticleRepository extends CrudRepository<Article, Long> {
 
 }
--- fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/UserRepository.java
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package fifty.shades.of.blush.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import fifty.shades.of.blush.entities.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Long> {
 
 	User findByUsername(String username);
