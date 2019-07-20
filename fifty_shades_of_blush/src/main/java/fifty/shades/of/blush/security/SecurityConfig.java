049b2756cc54a2cf556c5f169538b9d739acefe0
natan.daniel <natan.daniel0@gmail.com>
2019-07-20 18:10:37 +0200

    enabling crsf token

--- fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/security/SecurityConfig.java
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/security/SecurityConfig.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -14,10 +13,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
 
 @Configuration
 @EnableWebSecurity
@@ -38,19 +37,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.POST, "/api/articles").hasRole("ADMIN")
-				.antMatchers(HttpMethod.PUT, "/articles").hasRole("ADMIN").antMatchers(HttpMethod.PATCH, "/articles")
-				.hasRole("ADMIN").antMatchers(HttpMethod.DELETE, "/api/articles").hasRole("ADMIN")
-				.antMatchers("/api/users").hasRole("ADMIN").antMatchers("/", "/**").permitAll().and().cors();
+		http.cors().and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
+				.authorizeRequests().antMatchers("/**/*.{js,html,css}").permitAll().antMatchers("/", "/api/articles")
+				.permitAll().anyRequest().permitAll();
 	}
 
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		final CorsConfiguration configuration = new CorsConfiguration();
 		configuration.setAllowedOrigins(Collections.unmodifiableList(Arrays.asList("*")));
-		configuration.setAllowedMethods(Collections.unmodifiableList(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")));
+		configuration.setAllowedMethods(
+				Collections.unmodifiableList(Arrays.asList("HEAD", "GET", "POST", "PUT", "DELETE", "PATCH")));
 		configuration.setAllowCredentials(true);
-		configuration.setAllowedHeaders(Collections.unmodifiableList(Arrays.asList("Authorization", "Cache-Control", "Content-Type")));
+		configuration.setAllowedHeaders(Collections
+				.unmodifiableList(Arrays.asList("Authorization", "Cache-Control", "Content-Type", "X-XSRF-TOKEN")));
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
