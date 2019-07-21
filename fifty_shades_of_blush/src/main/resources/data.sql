769b9568b6e07671225e2c1a935431b39bbf3d3c
natan.daniel <natan.daniel0@gmail.com>
2019-07-21 12:41:55 +0200

    refactor

--- fifty_shades_of_blush/src/main/resources/data.sql
+++ fifty_shades_of_blush/src/main/resources/data.sql
@@ -3,17 +3,20 @@ delete from users;
 
 insert into users (id, username, password, first_name, last_name, created_at, updated_at) values(1, 'natandaniel', 'natandaniel', 'natan', 'daniel', SYSDATE, SYSDATE);
 
-insert into articles (id, title, subtitle, user_id, body, article_type, created_at, updated_at) 
-values (1, 'BEAUTY', 'THIS IS A BEAUTY ARTICLE', 1, 'THIS IS A BEAUTY ARTICLE', 'BEAUTY', SYSDATE, SYSDATE);
+insert into articles (id, title, subtitle,  body, article_type, created_at, updated_at) 
+values (1, 'BEAUTY', 'THIS IS A BEAUTY ARTICLE', 'THIS IS A BEAUTY ARTICLE', 'BEAUTY', SYSDATE, SYSDATE);
 
-insert into articles (id, title, subtitle, user_id, body, article_type, created_at, updated_at) 
-values (2, 'FASHION', 'THIS IS A FASHION ARTICLE', 1, 'THIS IS A FASHION ARTICLE', 'FASHION', SYSDATE, SYSDATE);
+insert into articles (id, title, subtitle,  body, article_type, created_at, updated_at) 
+values (2, 'FASHION', 'THIS IS A FASHION ARTICLE', 'THIS IS A FASHION ARTICLE', 'FASHION', SYSDATE, SYSDATE);
 
-insert into articles (id, title, subtitle, user_id, body, article_type, created_at, updated_at) 
-values (3, 'TRAVEL', 'THIS IS A TRAVEL ARTICLE', 1, 'THIS IS A TRAVEL ARTICLE', 'TRAVEL', SYSDATE, SYSDATE);
+insert into articles (id, title, subtitle,  body, article_type, created_at, updated_at) 
+values (3, 'TRAVEL', 'THIS IS A TRAVEL ARTICLE', 'THIS IS A TRAVEL ARTICLE', 'TRAVEL', SYSDATE, SYSDATE);
 
-insert into articles (id, title, subtitle, user_id, body, article_type, created_at, updated_at) 
-values (4, 'LIFESTYLE', 'THIS IS A LIFESTYLE ARTICLE', 1, 'THIS IS A LIFESTYLE ARTICLE', 'LIFESTYLE', SYSDATE, SYSDATE);
+insert into articles (id, title, subtitle,  body, article_type, created_at, updated_at) 
+values (4, 'LIFESTYLE', 'THIS IS A LIFESTYLE ARTICLE', 'THIS IS A LIFESTYLE ARTICLE', 'LIFESTYLE', SYSDATE, SYSDATE);
 
-insert into articles (id, title, subtitle, user_id, body, article_type, created_at, updated_at) 
-values (5, 'BEAUTY', 'THIS IS A 2ND BEAUTY ARTICLE', 1, 'THIS IS A 2ND BEAUTY ARTICLE', 'BEAUTY', SYSDATE, SYSDATE);
+insert into articles (id, title, subtitle,  body, article_type, created_at, updated_at) 
+values (5, 'BEAUTY', 'THIS IS A 2ND BEAUTY ARTICLE', 'THIS IS A 2ND BEAUTY ARTICLE', 'BEAUTY', SYSDATE, SYSDATE);
+
+insert into articles (id, title, subtitle,  body, article_type, created_at, updated_at) 
+values (6, 'BEAUTY', 'THIS IS A 3ND BEAUTY ARTICLE', 'THIS IS A 3ND BEAUTY ARTICLE', 'BEAUTY', SYSDATE, SYSDATE);
