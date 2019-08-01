b410ddd8579204475f40e1012fd7a6b40dfbaa7c
natandaniel <natan.daniel0@gmail.com>
2019-08-01 16:17:35 +0200

    updating loadUserByUsername to use optional and lambda

--- fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/security/UserRepositoryUserDetailsService.java
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/security/UserRepositoryUserDetailsService.java
@@ -1,9 +1,13 @@
 package fifty.shades.of.blush.security;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import fifty.shades.of.blush.entities.User;
@@ -21,10 +25,10 @@ public class UserRepositoryUserDetailsService implements UserDetailsService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		User user = userRepo.findByUsername(username);
-		if (user != null) {
-			return user;
-		}
-		throw new UsernameNotFoundException("User '" + username + "' not found");
+		
+        Objects.requireNonNull(username);
+        User user = userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return user;
 	}
 }
