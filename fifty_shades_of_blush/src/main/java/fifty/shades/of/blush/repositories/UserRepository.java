4ea420e826bd24709e2c17a737c083096443a028
natandaniel <natan.daniel0@gmail.com>
2019-08-01 16:15:06 +0200

    using Optional

--- fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/UserRepository.java
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/repositories/UserRepository.java
@@ -1,11 +1,13 @@
 package fifty.shades.of.blush.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import fifty.shades.of.blush.entities.User;
 
 public interface UserRepository extends PagingAndSortingRepository<User, Long>  {
 
-	User findByUsername(String username);
+	Optional<User> findByUsername(String username);
 
 }
