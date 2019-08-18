f51139ad321e89e0a84c1325a09a4828032a41c3
natan.daniel <natan.daniel0@gmail.com>
2019-08-18 15:06:33 +0200

    db file storgae service

---
+++ fifty_shades_of_blush/src/main/java/fifty/shades/of/blush/DBFileStorageService.java
@@ -0,0 +1,41 @@
+package fifty.shades.of.blush;
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import fifty.shades.of.blush.entities.ArticleFile;
+import fifty.shades.of.blush.repositories.ArticleFilesRepository;
+
+@Service
+public class DBFileStorageService {
+
+    @Autowired
+    private ArticleFilesRepository dbFileRepository;
+
+    public ArticleFile storeFile(MultipartFile file) throws FileStorageException {
+        // Normalize file name
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+        try {
+            // Check if the file's name contains invalid characters
+            if(fileName.contains("..")) {
+                throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
+            }
+
+            ArticleFile dbFile = new ArticleFile(fileName, file.getContentType(), file.getBytes());
+
+            return dbFileRepository.save(dbFile);
+        } catch (IOException ex) {
+            throw new FileStorageException("Could not store file " + fileName + ". Please try again!", ex);
+        }
+    }
+
+    public ArticleFile getFile(String fileId) throws MyFileNotFoundException {
+        return dbFileRepository.findById(fileId)
+                .orElseThrow(() -> new MyFileNotFoundException("File not found with id " + fileId));
+    }
+}
