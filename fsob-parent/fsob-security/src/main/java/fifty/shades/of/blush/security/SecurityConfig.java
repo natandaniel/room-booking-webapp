1442fcda6057871a3b677e864e4d009e332a279b
natan.daniel <natan.daniel0@gmail.com>
2019-08-22 23:36:43 +0200

    update /api/basicauth used for basic auth

--- fsob-parent/fsob-security/src/main/java/fifty/shades/of/blush/security/SecurityConfig.java
+++ fsob-parent/fsob-security/src/main/java/fifty/shades/of/blush/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -72,8 +73,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	        .csrf().disable()   
 	        
 	        .authorizeRequests()
+	        
+	        	.antMatchers("/api/basicauth").authenticated()
 	        	
-	            .anyRequest().authenticated()
+	        	.antMatchers(HttpMethod.GET).permitAll()
 	            
 	        .and()
 	            
